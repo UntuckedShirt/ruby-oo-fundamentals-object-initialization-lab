6da0bcfaab70734895adca5c32ba9185b8828130
Nathan Miller <natem31190@gmail.com>
2020-12-29 20:13:49 -0500

    commit tree 5141b60dc3b7d2d656e5d956be05620ef7faf088

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+     def initialize(name, breed="Mutt")
+        @name = name
+        @breed = breed
+    
+     end
+    
+    
+    
+        # end
+
+    # def breed("Mutt")
+    #     @breed = breed
+    #     end
+
+    #     def breed
+    #         @breed
+    # end
+end
